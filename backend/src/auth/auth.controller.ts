3860561604c27098cbbb43fab2df77d13a30401e
krishoang <kristinker.work@gmail.com>
2025-03-08 21:59:58 +0700

    connect frontend with backend server via proxy, builds a form to send POST request

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -5,10 +5,12 @@ import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('signin')
   signIn(@Body() dto: AuthDto) {
     return this.authService.signIn(dto);
   }
+
   @Post('signup')
   signUp(@Body() dto: AuthDto) {
     return this.authService.signUp(dto);
---
+++ frontend/src/components/login-form.tsx
@@ -0,0 +1,25 @@
+export default function LogInForm() {
+
+    const formHandler = (formData: FormData) => {
+        for (const i of formData.entries()) {
+            console.log(i)
+        }
+    }
+
+    return <form action={'/auth/signin'} method="post">
+        <label >
+            Email:
+            <input type="email" name="email" />
+        </label>
+        <label >
+            Password:
+            <input type="text" name="password" />
+        </label>
+        <label >
+            First Name:
+            <input type="text" name="firstName" />
+        </label>
+        <button type="submit">Submit</button>
+    </form>
+}
+
