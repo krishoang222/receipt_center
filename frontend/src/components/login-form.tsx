8d798f90c392862973ef93b8f9358faf40f167ff
krishoang <kristinker.work@gmail.com>
2025-03-09 01:51:23 +0700

    frontend: add form that submits with url encoded form

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -1,12 +1,33 @@
-export default function LogInForm() {
+import { useFormStatus } from "react-dom"
 
-    const formHandler = (formData: FormData) => {
-        for (const i of formData.entries()) {
-            console.log(i)
+export default function LogInForm() {
+    const SubmitButton = () => {
+        // (?) weird that useFormStatus's 'method' value return the value of the <form/> element, whose method prop is being overriden when action is a function. Meaning `method` always return 'get'  
+        const { pending } = useFormStatus();
+        return <button type="submit" disabled={pending} >{pending ? 'Submitting...' : 'Submit'}</button>
+    }
+    const submitHandler = (formData: FormData) => {
+        const convertFormDatatoJSON = (formData: FormData): Record<string, string> => {
+            // Source: https://stackoverflow.com/questions/35325370/how-do-i-post-a-x-www-form-urlencoded-request-using-fetch
+            // convert to JSON in order to use with `new URLSearchParams()`
+            const formDataJSON: Record<string, string> = {};
+            for (const [key, value] of formData.entries()) {
+                // hard code .toString() with assumption this form only have text inputs 
+                formDataJSON[key] = value.toString()
+            };
+            return formDataJSON
         }
+
+        fetch('/api/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams(convertFormDatatoJSON(formData))
+        })
     }
 
-    return <form action={'/api/auth/signin'} method="post">
+    return <form action={submitHandler}>
         <label >
             Email:
             <input type="email" name="email" />
@@ -19,7 +40,7 @@ export default function LogInForm() {
             First Name:
             <input type="text" name="firstName" />
         </label>
-        <button type="submit">Submit</button>
+        <SubmitButton />
     </form>
 }
 
