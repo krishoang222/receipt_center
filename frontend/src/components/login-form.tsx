a00878a55a5175891f27e5a34fc7caaec6b8c073
krishoang <kristinker.work@gmail.com>
2025-03-10 13:04:59 +0700

    add error handle for loggin

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -2,22 +2,34 @@ import { useActionState, useRef } from 'react';
 import { useFormStatus } from 'react-dom';
 
 export default function LogInForm() {
-  const [_, action] = useActionState(submitHandler, null);
+  const [message, action] = useActionState(submitHandler, 'Waiting for log in');
 
-  function submitHandler(_: any, formData: FormData) {
+  async function submitHandler(_: any, formData: FormData) {
+    // TODO: do I need to add `user server` here? why?
     // only allow formData with string values (not File)
     if (!Array.from(formData).every(([_, value]) => typeof value === 'string'))
       throw new Error('Form only accepts string');
 
-    fetch('/api/auth/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
+    try {
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // TODO: not know why TypeScript not catch the new type of formData after adding the guard clause (to only allow string values)
+        body: new URLSearchParams(formData),
+      });
 
-      // TODO: not know why TypeScript not catch the new type of formData after adding the guard clause (to only allow string values)
-      body: new URLSearchParams(formData),
-    });
+      // add noticeable 1s pause
+      await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+      if (!res.ok) throw new Error(`${res.status}-${res.statusText}`);
+
+      return 'Logged In Successfully';
+    } catch (error) {
+      return `Logged In Failed (status: ${error})`;
+    }
   }
 
   const SubmitButton = () => {
@@ -30,6 +42,7 @@ export default function LogInForm() {
       </button>
     );
   };
+
   return (
     <form action={action}>
       <label>
@@ -44,6 +57,7 @@ export default function LogInForm() {
         First Name:
         <input type="text" name="firstName" />
       </label>
+      <p>{message}</p>
       <SubmitButton />
     </form>
   );
